fc1fe767616a2de1d17752c800c2a7e77b6e84a1
Kien Truong <47377396+ktruong29@users.noreply.github.com>
2021-03-11 01:48:11 -0500

    Update F9-2_OutputAcctInfo.cpp

--- F9-2_OutputAcctInfo.cpp
+++ F9-2_OutputAcctInfo.cpp
@@ -23,21 +23,21 @@
 * 	==> Returns nothing
 ******************************************************************************/
 void OutputAcctInfo(string transType,	//IN - transaction type
-										string date,		//IN - date of transaction
-										int	   acctNum,		//IN - account number
-										string name,		//IN - client's name
-										float  amount,		//IN - amount of transaction
-										float  balance)	    //IN - balance after each transaction
+		    string date,	//IN - date of transaction
+		    int	   acctNum,	//IN - account number
+		    string name,	//IN - client's name
+		    float  amount,	//IN - amount of transaction
+		    float  balance)	//IN - balance after each transaction
 {
-	cout << left  << setw(TRANS_COL)			<< transType
-							  << setw(DATE_COL)   		<< date
-							  << setw(ACCT_COL)	  		<< acctNum
-							  << setw(NAME_COL)   		<< name			<< '$';
+	cout << left  << setw(TRANS_COL)	<< transType
+		      << setw(DATE_COL)   	<< date
+		      << setw(ACCT_COL)	  	<< acctNum
+		      << setw(NAME_COL)   	<< name	<< '$';
 
 	cout << setprecision(2) << fixed;
 
-	cout << right << setw(AMOUNT_COL - 1) 	<< amount 		<< "   " << '$'
-				  			<< setw(BAL_COL - 1 )	  	<< balance		<< endl;
+	cout << right << setw(AMOUNT_COL - 1) 	<< amount  << "   " << '$'
+		      << setw(BAL_COL - 1 )	<< balance << endl;
 
 	cout << setprecision(6);
 	cout.unsetf(ios::fixed);
